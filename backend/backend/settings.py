ea58e30d391f61c43691498d81b85ad728cd7e31
carelesswhisker <muringjohncarlo@gmail.com>
2024-12-12 19:20:57 +0800

    first commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -99,11 +99,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'carelesswhisker',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD' : os.getenv("DB_PWD"),
+        'HOST' : os.getenv("DB_HOST"),
+        'PORT' : os.getenv("DB_PORT"),
     }
 }
 
